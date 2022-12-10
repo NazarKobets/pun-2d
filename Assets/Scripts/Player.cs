483792f3af75c79783038561a4cf3cd763658577
NazarKobets <66803921+NazarKobets@users.noreply.github.com>
2022-12-10 10:59:29 +0200

    Modified Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public float speed;
+
     private Animator anim;
+    private PhotonView view;
 
     private Vector3 initialScale;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        view = GetComponent<PhotonView>();
+
         initialScale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+        if (view.IsMine)
+        {
+            Move();
+        }
     }
 
     void Move()
@@ -38,7 +46,7 @@ public class Player : MonoBehaviour
         transform.localScale = moveInput.x switch
         {
             < 0 => new Vector3(-initialScale.x, initialScale.y, initialScale.z),
-            > 0 => new Vector3(-initialScale.x, initialScale.y, initialScale.z),
+            > 0 => new Vector3(initialScale.x, initialScale.y, initialScale.z),
             _ => transform.localScale
         };
     }
